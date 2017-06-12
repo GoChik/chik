31d7c463f1968f87d34027c9ea4368f06d423571
Riccardo Ferrazzo <rferrazz@develer.com>
2017-06-12 10:16:54 +0200

    Update commands definitions

--- commands.go
+++ commands.go
@@ -10,24 +10,38 @@ const (
 	GET_STATUS
 )
 
+// TimedCommand represent a command with an associated delay in minutes
+// if TimerID is zero it means id has not been set
+type TimedCommand struct {
+	TimerID      uint16      `json:",int"`
+	Command      CommandType `json:",int"`
+	DelayMinutes int         `json:",int"`
+}
+
 // DigitalCommand used to instruct the appliance to execute
-// an action on a gpio pin
+// a TimedCommand on a gpio pin
 type DigitalCommand struct {
-	Pin     int         `json:",string"`
-	Command CommandType `json:",string"`
+	TimedCommand
+	Pin int `json:",int"`
 }
 
 // StatusIndication is the response to a status request Command
 // Pin indicates the pin which status refers to
 // Status is the current value
 type StatusIndication struct {
-	Pin    int
-	Status bool
+	Pin    int  `json:",int"`
+	Status bool `json:",bool"`
+	Timers []TimedCommand
 }
 
 // AIInfoMessage sent from the android application to inform
 // when user came home and what appliances are enabled
 type AIInfoMessage struct {
-	WiFiStatus    bool
-	AIEnabledPins []int
+	EnabledPins []int
+	AtHome      bool
+}
+
+type AIConfigurationMessage struct {
+	timeSpan        int
+	collectInterval int
 }
