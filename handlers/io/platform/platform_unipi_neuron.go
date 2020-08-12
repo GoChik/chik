ff5439e471f0bb1870f8d778ef8f8ded0d55082f
Riccardo Ferrazzo <f.riccardo87@gmail.com>
2020-08-12 16:59:25 +0200

    Enable modbus on unipi neuron platform

--- handlers/io/platform/platform_unipi_neuron.go
+++ handlers/io/platform/platform_unipi_neuron.go
@@ -6,8 +6,14 @@ import (
 	"github.com/gochik/chik/handlers/io/bus/softbus"
 	"github.com/gochik/chik/handlers/io/bus/unipibus"
 	"github.com/gochik/chik/handlers/io/bus/w1bus"
+	"github.com/gochik/chik/handlers/io/bus/modbus"
 )
 
 func init() {
-	initializeBus(softbus.New, unipibus.New, w1bus.New)
+	initializeBus(
+		softbus.New,
+		unipibus.New,
+		w1bus.New,
+		modbus.New,
+	)
 }
